2110679aa4a1a5a846b663e9784d9194933445cd
Vidya Nyayapati <vidya.nyayapati@gmail.com>
2024-01-09 19:15:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    """app.run(host='0.0.0')"""
+    app.run()
