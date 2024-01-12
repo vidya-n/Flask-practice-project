33e09978e42781653ad87d0676da46cc633c7d03
Vidya Nyayapati <vidya.nyayapati@gmail.com>
2024-01-12 14:59:53 +0530

    Update for requests

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
 if __name__ == '__main__':
-    print_hi('Vidya')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
+    """app.run(host='0.0.0')"""
+    app.run()
