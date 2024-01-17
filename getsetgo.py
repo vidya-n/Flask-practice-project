427ffdb3b9abedb2f4476690018336d932016813
Vidya Nyayapati <vidya.nyayapati@gmail.com>
2024-01-17 19:21:31 +0530

    Trying the post method

--- getsetgo.py
+++ getsetgo.py
@@ -5,11 +5,25 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return <html><body><h1> Welcome to homepage</h1></body></html>
-    
 
-@app.route('/home/<username>')
-def username_display(username):
-    return username
+@app.route(rule='/user', methods = ['GET'])
+def get_user():
+    return '''
+        <form method="POST">
+        <div><label> Enter first name: <input type='text' name='firstname'> </label></div>
+        <div><label> Enter last name: <input type='text' name='lastname'> </label></div>
+        <input type='submit' value='Submit'>
+        </form>'''
+
+
+@app.route(rule='/user', methods = ['POST'])
+def username_display():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    name = {1: firstname, 2: lastname}
+    return '''
+    <html><body><h1> The name associated with {1} is: {2} </h1></body></html>'''.format_map(name)
+
 
 
 if __name__ == '__main__':
